ac149de3977037a4c593f51d92cb4f6661eab92e
Ida Darmochwał <ida.mdart@gmail.com>
2022-10-25 23:22:32 +0200

    Modify package.json

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,6 +4,5 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
